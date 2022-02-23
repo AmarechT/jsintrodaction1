8737875d5e48b13d105a316f9e360e7d07c78c13
Amarecht <Amarechtato22@gmail.com>
2022-02-23 00:39:03 -0600

    creat indexjs

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("AmarechT")//this is used to print AmarechT
+let a=5;          //decler avareable a and assign number5
+let b=6;          //declar avareable b and assign number 6
+console.log(a+b)
+console.log(b-a)
+console.log(a*b)
